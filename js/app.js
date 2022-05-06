68a999f02bc414a43a6682944d826fd720afcb78
MARIO ACEVEDO <JUN_ACEVEDO415@YAHOO.COM>
2022-05-05 22:43:21 -0700

    fixed yes or no questions and fixed alert at end

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 let username = prompt("What is your name?");
 alert("Hello " + username + "! we are going to play a game.");
 alert("I am going to ask you some questions about me");
-alert("You will tell me if they are true or false");
+alert("You will answer with a yes or no");
 
 
 
@@ -14,23 +14,23 @@ else if (answer.toLowerCase() === "no") alert("Wrong Of Course I Like Sports!");
 
 let answer2 = prompt("Do I Love Insects?");
 if (answer2.toLowerCase() === "no") alert("You are correct I hate bugs!");
-else if (answer2.toLowerCase() === "yes") alert("No way bugs are gross!");
+else if (answer2.toLowerCase() === "yes") alert("That is not correct I dont like bugs!");
 // console.log(answer2);
 
 let answer3 = prompt("Is my favorite food steak?");
 if (answer3.toLowerCase() === "yes") alert("Yes you are correct I love steak!");
-else if (answer3.toLowerCase() === "no") alert("Everyone loves steak!");
+else if (answer3.toLowerCase() === "no") alert("That is no not correct I love steaks!");
 // console.log(answer3);
 
-let answer4 = prompt("Am I a scorpio?");
-if (answer4.toLowerCase() === "no") alert("You are correct I am not a scorpio");
-else if (answer4.toLowerCase() == "yes") alert("I am not a scorpio");
+let answer4 = prompt("Am I am Aries?");
+if (answer4.toLowerCase() === "yes") alert("You are correct I am an aries");
+else if (answer4.toLowerCase() == "no") alert("That is not correct I am an aries");
 // console.log(answer4);
 
 let answer5 = prompt("Do I have children?");
-if (answer5.toLowerCase() === "yes") alert("yes I have 2 daughters");
+if (answer5.toLowerCase() === "yes") alert("you are correct I have 2 daughters");
 else if (answer5.toLowerCase() === "no")
   alert("That is incorrect I have 2 daughters");
 // console.log(answer5);
 
-let goodbye = alert("Goodbye Mario Thanks For Playing!")
+let goodbye = alert(" Goodbye " +  username + " Thanks For Playing!")
